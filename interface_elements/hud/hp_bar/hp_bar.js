331acfca0a14fb8f3e3e53e27b7c1d350edb1599
noh8rz <melonzipchannelda@gmail.com>
2023-09-15 23:27:19 +0700

    Добавил комментов

--- interface_elements/hud/hp_bar/hp_bar.js
+++ interface_elements/hud/hp_bar/hp_bar.js
@@ -1,11 +1,13 @@
 class hp_bar{
 
+	//задаём начальное количество хп условно 100 или любое другое значение
 	constructor(amount){
 		this.hp = amount;
 		this.amount = amount;
 		this.bar = document.querySelectorAll('.bar')[0];
 	}
 
+	//функция чтобы задамажить на количество
 	damage(dmg){
 		if(this.hp>dmg)
 		{
@@ -19,6 +21,7 @@ class hp_bar{
 		}
 	}
 
+	//функция чтобы полечить на количество
 	heal(amount){
 		if(this.hp<this.amount-amount)
 		{
@@ -32,6 +35,7 @@ class hp_bar{
 		}
 	}
 
+	//обновление хп бара
 	update_bar(){
 		document.querySelectorAll('.bar')[0].style.width = this.hp*100/this.amount + "%";
 		console.log(this.hp);
