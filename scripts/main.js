54bdbad2bc3066fab7ecb04f5d5db77b50fc7b7f
list91 <s_anufriev@list.ru>
2023-09-28 17:49:39 +0700

    создал 3 типа оружия

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,14 @@ import Enemy from './enemies/enemy.js';
 import Player from './hero/hero.js'
 
 const hero = new Player;
+// title, bulletSpeed, oneShotCounts, maxBullets, rechargeSpeed, damage, radiusSpeed
+const revolver = new Gun("Пистолет", 500, 1, 15, 10, [10, 20], 0.02);
+const rifle = new Gun("Автомат", 100, 1, 32, 30, [5, 10], 0.1);
+const shotgun = new Gun("Ружье", 500, 8, 3, 10, [20, 30], 0.5);
+
+const hero = new Player(revolver);
 const playerObject = hero.player;
+const guns = [revolver, rifle, shotgun];
 
 const enemy1 = new Enemy(100, 100);
 const enemy2 = new Enemy(100, 100);
